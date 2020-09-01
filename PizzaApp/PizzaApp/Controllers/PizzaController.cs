746d628c884a992849c1d539fc7964b7d16039d5
Majjja <45804941+Majjja@users.noreply.github.com>
2020-09-01 12:35:42 +0200

    commit

---
+++ PizzaApp/PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PizzaApp.Models;
+
+namespace PizzaApp.Controllers
+{
+    [Route("pizzas")]
+    public class PizzaController : Controller
+    {
+        [HttpGet("list")]
+        public IActionResult Index()
+        {
+            List<PizzaModel> pizzas = StaticDB.ListOfPizzas;
+            return View(pizzas);
+        }
+
+        [HttpGet("create")]
+        public IActionResult CreatePizza()
+        {
+            return View();
+        }
+
+        [HttpPost("createNew")]
+        public IActionResult CreateNewPizza(PizzaModel model)
+        {
+            var newItem = new PizzaModel()
+            {
+                Id = StaticDB.ListOfPizzas.Count + 1,
+                Name = model.Name,
+                Size = model.Size,
+                Price = model.Price
+            };
+
+            StaticDB.ListOfPizzas.Add(newItem);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("details/{id}")]
+        public IActionResult PizzaDetails(int id)
+        {
+            var pizza = StaticDB.ListOfPizzas.SingleOrDefault(x => x.Id == id);
+            return View(pizza);
+        }
+
+        [HttpGet("delete/{id}")]
+        public IActionResult PizzaDelete(int id)
+        {
+            var pizza = StaticDB.ListOfPizzas.SingleOrDefault(x => x.Id == id);
+            return View(pizza);
+        }
+
+        [HttpDelete("deletePizza/{id}")]
+        public IActionResult DeletePizzaFromList(int id)
+        {
+            var pizza = StaticDB.ListOfPizzas.SingleOrDefault(x => x.Id == id);
+            StaticDB.ListOfPizzas.Remove(pizza);
+            return RedirectToAction("Index");            
+        }
+    }
+}
---
+++ PizzaApp/PizzaApp/StaticDB.cs
@@ -0,0 +1,19 @@
+﻿using PizzaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaApp
+{
+    public static class StaticDB
+    {
+        public static List<PizzaModel> ListOfPizzas = new List<PizzaModel>()
+        {
+            new PizzaModel(1, "Capricciosa", PizzaSize.Family, 200 * 1.5),
+            new PizzaModel(2, "Cheese", PizzaSize.Medium, 305),
+            new PizzaModel(3, "Veggie", PizzaSize.Small, 180 * 0.75),
+            new PizzaModel(4, "Margherita", PizzaSize.Family, 250 * 1.5),
+        };
+    }
+}
